08f425e97cefecdd2d90f199c0dcaa1a72062835
Tiago Sales <tiagosales@outlook.com>
2024-12-16 19:27:05 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# weather-otel-goexpert
+Sistema distribuido em 2 serviços que retornam o clima atual baseado em um CEP informado
+
+# Desafio GOLang Observabilidade com trace distribuído - Consulta Temperatura baseado em um CEP informado - FullCycle 
+
+Aplicação em Go sendo: 
+  - Servidor HTTP Rest Client
+  - Servidor HTTP Rest Server
+  - Servidor Zipkin para apresentação do trace distribuído
+  - Servidor Jaeger para apresentação do trace distribuído
+  - Servidor Prometheus
+  - Servidor Opentelemetry
+  - Servidor Grafana
+
+&nbsp;
+- **Aplicação em Container com - Docker-compose e Dockerfile**
+
+## Funcionalidades
+
+- **Consulta de Temperatura com retorno Localidade, Celsius, Kelvin e Fahrenheit**
+  - O servidor permite consultar a temperatura informando um CEP.
+  - Retorno esperado:
+```
+  {
+  "city": "São Paulo",
+	"temp_C": 22.3,
+	"temp_F": 72.1,
+	"temp_K": 295.3
+  } 
+``` 
+  - Sendo city = A cidade do cep informado
+  - Sendo temp_F = Fahrenheit
+  - Sendo temp_C = Celsius
+  - Sendo temp_K = Kelvin  
+
+## Como Utilizar localmente:
+
+1. **Requisitos:** 
+   - Certifique-se de ter o Go instalado em sua máquina.
+   - Certifique-se de ter o Docker instalado em sua máquina.
+   - Foi atulizado a API viaCEP para encontrar a localização que deseja consultar a temperatura: https://viacep.com.br/
+   - Foi utilizado a API WeatherAPI para consultar as temperaturas desejadas: https://www.weatherapi.com/
+
+
+&nbsp;
+2. **Clonar o Repositório:**
+&nbsp;
+
+```bash
+git clone https://github.com/tiago-g-sales/weather-otel-goexpert.git
+```
+&nbsp;
+3. **Acesse a pasta do app:**
+&nbsp;
+
+```bash
+cd weather-otel-goexpert
+```
+&nbsp;
+4. **Rode o docker-compose para buildar e executar toda a stack de observabilidade:**
+&nbsp;
+
+```bash 
+ docker-compose up
+```
+
+&nbsp;
+
+
+## Como testar localmente:
+
+### Portas
+HTTP server on port :8080 <br />
+
+### HTTP
+ - Execute o curl abaixo ou use um aplicação client REST para realizar a requisição.   
+  curl --request POST \
+  --url http://localhost:8080/ \
+  --header 'Content-Type: application/json' \
+  --header 'User-Agent: insomnia/10.0.0' \
+  --data '{
+	"cep": "04911000"
+}'
+
+&nbsp;
+5. **Acessar o Zipkin para consulta do trace distribuído:**
+
+  - http://localhost:9411/
+
+&nbsp;
+6. **Acessar o Jaeger para consulta do trace distribuído:**
+
+  - http://localhost:16686/ 
+
+&nbsp;
+7. **Acessar o Grafana para consulta do trace distribuído:**
+
+  - http://localhost:3001/
+
+&nbsp;
+8. **Acessar o Prometheus para consulta do trace distribuído:**
+
+  - http://localhost:9090/
+  
---
+++ client/internal/web/server.go
@@ -0,0 +1,148 @@
+package web
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/tiago-g-sales/rate-limiter-goexpert/client/internal/model"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+)
+
+
+
+type Webserver struct {
+	TemplateData *TemplateData
+}
+
+// NewServer creates a new server instance
+func NewServer(templateData *TemplateData) *Webserver {
+	return &Webserver{
+		TemplateData: templateData,
+	}
+}
+
+// createServer creates a new server instance with go chi router
+func (we *Webserver) CreateServer() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Timeout(60 * time.Second))
+	// promhttp
+	router.Handle("/metrics", promhttp.Handler())
+	router.Get("/", we.HandleRequest)
+	return router
+}
+
+type TemplateData struct {
+	Title              string
+	ResponseTime       time.Duration
+	ExternalCallMethod string
+	ExternalCallURL    string
+	Content            string
+	RequestNameOTEL    string
+	OTELTracer         trace.Tracer
+}
+
+const(
+	INVALID_ZIP_CODE = "invalid zipcode"
+	NOTFOUND_ZIP_COD = "can not find zipcode"
+	LEN_ZIP_CODE = 8
+)
+
+func (h *Webserver) HandleRequest(w http.ResponseWriter, r *http.Request) {
+
+	carrier := propagation.HeaderCarrier(r.Header)
+	ctx := r.Context()
+	ctx = otel.GetTextMapPropagator().Extract(ctx, carrier)
+
+	ctx, spanInicial := h.TemplateData.OTELTracer.Start(ctx, "SPAN_INICIAL "+h.TemplateData.RequestNameOTEL)
+	spanInicial.End()
+
+	ctx, span := h.TemplateData.OTELTracer.Start(ctx, "Chamada externa "+h.TemplateData.RequestNameOTEL)
+	defer span.End()
+
+
+	if h.TemplateData.ExternalCallURL != "" {
+		var req *http.Request
+		var err error
+
+		var dto model.Cep
+		err = json.NewDecoder(r.Body).Decode(&dto)
+		if err != nil {
+			http.Error(w, INVALID_ZIP_CODE, http.StatusUnprocessableEntity)
+			return
+		}
+	
+		if dto.Cep == ""{
+			http.Error(w, INVALID_ZIP_CODE , http.StatusUnprocessableEntity )
+			return
+		}
+	
+		if len(dto.Cep) > LEN_ZIP_CODE || len(dto.Cep) < LEN_ZIP_CODE  {
+			http.Error(w, INVALID_ZIP_CODE , http.StatusUnprocessableEntity )
+			return		
+		}
+
+
+		if h.TemplateData.ExternalCallMethod == "GET" {
+			req, err = http.NewRequestWithContext(ctx, "GET", h.TemplateData.ExternalCallURL, nil)
+		} else if h.TemplateData.ExternalCallMethod == "POST" {
+			req, err = http.NewRequestWithContext(ctx, "POST", h.TemplateData.ExternalCallURL, nil)
+		} else {
+			http.Error(w, "Invalid ExternalCallMethod", http.StatusInternalServerError)
+			return
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(req.Header))
+		
+		h := http.Client{}
+
+		q := req.URL.Query() 
+		q.Add("cep", dto.Cep)
+		req.URL.RawQuery = q.Encode()	
+		resp, err := h.Do(req)
+		if err != nil {
+			http.Error(w, NOTFOUND_ZIP_COD, http.StatusNotFound) 
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+	
+		temp := model.Temperatura{}
+
+		err= json.Unmarshal(body, &temp)
+		if err != nil{
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(temp)
+
+	
+
+	
+
+	}
+
+
+}
